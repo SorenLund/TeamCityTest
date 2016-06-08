337be36ada69bcf6d35617f274ef0fd9e366e80f
Søren Lund <slu@mcb.dk>
2016-06-08 09:06:57 +0200

    test

---
+++ Calculator.Lib/Calculator.Lib/CalculatorLib.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Lib
+{
+    public class CalculatorLib
+    {
+        public int Add(int a, int b)
+        {
+            var res = a + b;
+            return res;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            var res = a - b;
+            return res;
+        }
+    }
+}
